98e3212b88700ff6c0ecb35f9f0b7802f85cdf22
Brandon Lind <linddeveloper@gmail.com>
2017-04-01 11:15:19 -0500

    BL: Upgrading express and using my standard simple node server bootstrapping

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-var http = require('http');
-var path = require('path');
-var express = require('express');
+var http = require('http'),
+    express = require('express'),
+    path = require('path'),
+    app = express(),
+    apiServer = require('./api/server.js'),
+    port = process.env.PORT,
+    host = process.env.IP,
+    publicFolderPath;
 
-var router = express();
-var server = http.createServer(router);
+http.createServer(app).listen(port, host);
+console.log('HTTP listening at ' + host + ' on port ' + port);
 
-router.use(express.static(path.resolve(__dirname, 'client')));
+publicFolderPath = path.resolve('./public');
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Server listening at", addr.address + ":" + addr.port);
+app.use('/api', apiServer); // Mount the HTTP API on the URL space /api
+app.use(express.static(publicFolderPath)); // For other requests, just serve /public
+app.get('/*', function(req, res) {
+    res.sendFile(path.resolve(publicFolderPath, 'index.html'));
 });
