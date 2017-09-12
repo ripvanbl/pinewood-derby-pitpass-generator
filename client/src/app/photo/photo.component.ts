5acfe3ef27c309ca0bd03f9c6c737d8c70dc7382
Brandon Lind <linddeveloper@gmail.com>
2017-09-12 02:04:49 +0000

    BL: Adding photo resize capability

--- client/src/app/photo/photo.component.ts
+++ client/src/app/photo/photo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Output, EventEmitter, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Output, ViewChild, ElementRef, Renderer2, EventEmitter, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import $ from 'jquery/dist/jquery';
 
 @Component({
   selector: 'app-photo',
@@ -7,39 +8,50 @@ import { Component, OnInit, Output, EventEmitter, ChangeDetectionStrategy, Chang
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PhotoComponent implements OnInit {
-  private trans1x1: string; 
+  private trans1x1: string;
+  private cardWidth: number;
   preview: any;
   isProcessing: boolean;
 
   @Output() onPhotoProcessing = new EventEmitter<void>();
   @Output() onPhotoProcessed = new EventEmitter<void>();
-
-
-  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+  @ViewChild('card') card: ElementRef;
+  
+  constructor(private changeDetectorRef: ChangeDetectorRef, private renderer: Renderer2) {}
 
   ngOnInit() {
     this.trans1x1 = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'; 
     this.preview = this.trans1x1;
     this.isProcessing = false;
   }
-
+  
+  ngAfterViewInit() {
+    this.cardWidth = this.card.nativeElement.clientWidth;
+  }
+  
   fileChange(input) {
     this.isProcessing = true;
     this.onPhotoProcessing.emit();
 
     this
       .readFiles(input.files)
-      .then((dataUrl) => { this.resizeFile(dataUrl); })
-      .catch(() => { 
+      .then((dataUrl) => { this.resizePhoto(dataUrl); })
+      .then(() => { 
+        setTimeout(
+          () => { this.changeDetectorRef.detectChanges(); },
+          1000
+        );
+      })
+      .catch((err) => { 
         this.preview = this.trans1x1; 
-        console.log('Something went wrong'); 
+        console.warn('Photo: Something went wrong', err);
       })
       .then(() => { 
         this.isProcessing = false; 
         this.onPhotoProcessed.emit(); 
       });
   }
-
+  
   private readFiles(files) {
     return new Promise((resolve, reject) => {
 
@@ -47,7 +59,7 @@ export class PhotoComponent implements OnInit {
 
       // Make sure there is a file
       if(!files || !files[0]) {
-        reject('No file to read.');
+        reject('Photo: No file to read.');
         return;
       }
 
@@ -63,11 +75,25 @@ export class PhotoComponent implements OnInit {
     
   }
 
-  private resizeFile(dataUrl) {
-    setTimeout(() => {
-    this.preview = dataUrl;
-    this.changeDetectorRef.detectChanges();
-    }, 2000);
+  private resizePhoto(dataUrl) {
+    let img = new Image();
+    
+    // Wait for the data to load before calculating
+    img.onload = () => {
+      let originalHeight = img.naturalHeight;
+      let originalWidth = img.naturalWidth;
+      let aspectRatio = originalWidth / originalHeight;
+      let width = this.cardWidth;
+      let height = this.cardWidth * originalHeight / originalWidth; //width * aspectRatio;
+      let canvas = this.renderer.createElement('canvas');
+      
+      canvas.width = width;
+      canvas.height = height;
+      canvas.getContext('2d').drawImage(img, 0, 0, width, height);
+      this.preview = canvas.toDataURL("image/png");
+    };
+    
+    // Set the source to the file
+    img.src = dataUrl;
   }
-
 }
