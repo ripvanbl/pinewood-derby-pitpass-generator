d480d2ca6ff1287e31f39f7feb1a4ec0079ddf3c
Brandon Lind <linddeveloper@gmail.com>
2017-09-17 17:41:19 +0000

    BL: Style clean up to adjust to new angular material classes

--- client/src/app/scout/scout.model.ts
+++ client/src/app/scout/scout.model.ts
@@ -1,7 +1,21 @@
 export const ranks = ['Tiger', 'Wolf', 'Bear', 'Webelos', 'Arrow of Light'];
 
-export class Scout {
+export class Racer {
+  private trans1x1: string = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
   firstname: string;
   lastname: string;
   rank: string;
+  carname: string;
+  profilePhotoDataURL: string;
+  
+  constructor() {
+    this.profilePhotoDataURL = this.trans1x1;
+  }
+  
+  reset() {
+    this.firstname = '';
+    this.lastname = '';
+    this.rank = '';
+    this.profilePhotoDataURL = this.trans1x1;
+  }
 };
