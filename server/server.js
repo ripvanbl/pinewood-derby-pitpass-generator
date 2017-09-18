1c6392f776e21653c7f72ae5c7e1508697f45f90
Brandon Lind <linddeveloper@gmail.com>
2017-09-18 02:56:27 +0000

    BL: More changes for Heroku deployment. Forces to https if in production mode and request isn't for https

--- server/server.js
+++ server/server.js
@@ -10,7 +10,27 @@ var http = require('http'),
 http.createServer(app).listen(port, host);
 console.log('HTTP listening at ' + host + ' on port ' + port);
 
-publicFolderPath = path.resolve(__dirname, 'public/dist');
+publicFolderPath = path.resolve(__dirname, '../client/dist');
+
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 
 app.use('/api', apiServer); // Mount the HTTP API on the URL space /api
 app.use(express.static(publicFolderPath)); // For other requests, just serve /public
