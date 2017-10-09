d4331194ee0cd4c7d68be37c1bf04609f9edaea4
Brandon Lind <linddeveloper@gmail.com>
2017-10-08 19:02:14 -0500

    BL: Adding theme card with profile pic placeholder

--- client/src/app/themes/themes.component.ts
+++ client/src/app/themes/themes.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Racer } from '../racer/racer.model';
+import { RacerService } from '../racer/racer.service';
+
 @Component({
   selector: 'app-themes',
   templateUrl: './themes.component.html',
   styleUrls: ['./themes.component.css']
 })
 export class ThemesComponent implements OnInit {
+  public racer: Racer;
 
-  constructor() { }
+  constructor(private racerService: RacerService) { }
 
   ngOnInit() {
+    this.racer = this.racerService.racer;
   }
 
 }
