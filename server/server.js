796b90f8ee312302defadf114c179d92dc5c8c5a
Brandon Lind <linddeveloper@gmail.com>
2017-10-09 12:29:59 -0500

    BL: Updating start up logic for Heroku issue

--- server/server.js
+++ server/server.js
@@ -4,16 +4,18 @@ var http = require('http'),
     app = express(),
     apiServer = require('./api/index.js'),
     port = process.env.PORT || 8080,
-    host = process.env.IP || 'localhost',
     publicFolderPath;
 
-http.createServer(app).listen(port, host);
-console.log('HTTP listening at ' + host + ' on port ' + port);
-
 publicFolderPath = path.resolve(__dirname, '../client/dist');
+console.log('Serving static content from:', publicFolderPath);
 
 app.use('/api', apiServer); // Mount the HTTP API on the URL space /api
 app.use(express.static(publicFolderPath)); // For other requests, just serve /public
+
 app.get('/*', function(req, res) {
-    res.sendFile(path.resolve(publicFolderPath, 'index.html'));
+  res.sendFile(path.resolve(publicFolderPath, 'index.html'));
+});
+
+http.createServer(app).listen(port, function() {
+  console.log('HTTP listening at port', port);
 });
