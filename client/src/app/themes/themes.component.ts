6d216cc6b058d4679d851c4f0087e02682dd89f1
Brandon Lind <linddeveloper@gmail.com>
2017-10-08 21:31:08 -0500

    BL: Adding text sections to theme card

--- client/src/app/themes/themes.component.ts
+++ client/src/app/themes/themes.component.ts
@@ -10,11 +10,13 @@ import { RacerService } from '../racer/racer.service';
 })
 export class ThemesComponent implements OnInit {
   public racer: Racer;
+  public currentDate: Date;
 
   constructor(private racerService: RacerService) { }
 
   ngOnInit() {
     this.racer = this.racerService.racer;
+    this.currentDate = new Date();
   }
 
 }
